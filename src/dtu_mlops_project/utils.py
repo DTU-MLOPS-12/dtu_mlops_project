119f1084f378fdc1c3732f7b1e5da3e4dd34aee6
Veiglin <nicolaiveiglin@gmail.com>
2025-01-09 10:30:07 +0100

    Add utility function to select appropriate computation device

---
+++ src/dtu_mlops_project/utils.py
@@ -0,0 +1,15 @@
+import torch
+
+def get_device() -> str:  
+    """  
+    Selects the appropriate available device for performing computations.  
+    
+    :returns: The name of the device as a string  
+    """  
+    if torch.cuda.is_available():  
+        return "cuda"  
+    elif torch.backends.mps.is_available():
+        return "mps"  
+    else:  
+        return "cpu"
+    
