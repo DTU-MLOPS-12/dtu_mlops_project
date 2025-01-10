da33fb223d826af6a374bd84e8e8ea7cd4deaa20
HackTheOxidation <tomas.hagenau@protonmail.ch>
2025-01-10 10:57:05 +0100

    A basic API for running a model on an image

--- src/dtu_mlops_project/api.py
+++ src/dtu_mlops_project/api.py
@@ -1,7 +1,26 @@
+from contextlib import asynccontextmanager
 import fastapi
 from http import HTTPStatus
+from PIL import Image
 
-app = fastapi.FastAPI()
+from . import model
+
+
+@asynccontextmanager
+async def lifespan(app: fastapi.FastAPI):
+    print("API is up and running...")
+    yield
+    print("Shutting down API")
+
+
+# The FastAPI app object
+app = fastapi.FastAPI(lifespan=lifespan)
+
+# A base response for indicating OK requests.
+HTTP_200_OK = {
+    'message': HTTPStatus.OK.phrase,
+    'status-code': HTTPStatus.OK
+}
 
 
 @app.get('/')
@@ -9,7 +28,43 @@ def home():
     """
     Root end-point (for health-check purposes)
     """
-    return {
-        'message': HTTPStatus.OK.phrase,
-        'status-code': HTTPStatus.OK
+    return HTTP_200_OK
+
+
+@app.get('/about')
+def about():
+    """
+    A small 'about' section
+    """
+    return HTTP_200_OK | {
+        'model_name': 'mobilenetv4_conv_small.e2400_r224_in1k',
+        'base_model_url': '',
+        'repository_url': 'https://github.com/HackTheOxidation/dtu_mlops_project',
+        'dataset_name': '',
+        'dataset_url': ''
     }
+
+
+@app.post("/api/predict")
+def api_predict(image_file: fastapi.UploadFile | None = None):
+    """
+    API endpoint that receives an image file an runs the dummy
+    model through it.
+    """
+    if not image_file:
+        return {
+            'message': HTTPStatus.BAD_REQUEST.phrase,
+            'status': HTTPStatus.BAD_REQUEST,
+        }
+
+    image = Image.open(image_file)
+    if image.mode != 'RGB':
+        image = image.convert(mode='RGB')
+
+    probs, classes = model.dummy_model(image)
+
+    return HTTP_200_OK | {
+        'probabilities': probs,
+        'classification_indices': classes,
+    }
+
--- src/dtu_mlops_project/model.py
+++ src/dtu_mlops_project/model.py
@@ -1,3 +1,8 @@
+import functools
+import typing
+
+import timm
+import torch
 import typer
 
 app = typer.Typer()
@@ -8,6 +13,29 @@ def model() -> None:
     """
     (Placeholder) Function for running the model.
     """
+
+
+@functools.cache
+def dummy_model(image, k: int = 5) -> typing.Tuple:
+    """
+    Initializes a 'dummy' model ('mobilenetv4')
+    and runs it on an image (for temporary use).
+
+    :param image: The image to run the model on.
+    :param k: The number of classes to return.
+    :returns: a tuple of top 'k' class indices and their probabilities.
+    """
+    model_name = 'mobilenetv4_conv_small.e2400_r224_in1k'
+    mobilenetv4_model = timm.create_model(model_name, pretrained=True)
+    mobilenetv4_model.eval()
+
+    data_config = timm.data.resolve_model_data_config(mobilenetv4_model)
+    transforms = timm.data.create_transform(**data_config, is_training=False)
+
+    output = mobilenetv4_model(transforms(image).unsqueeze(0))
+
+    probabilities, class_indices = torch.topk(output.softmax(dim=1) * 100, k=k)
+    return probabilities, class_indices
 
 
 if __name__ == '__main__':
