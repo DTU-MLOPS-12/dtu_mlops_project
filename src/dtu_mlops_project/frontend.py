770d1cdf79e37716707194f10f00ee0f26c37ba8
Veiglin <nicolaiveiglin@gmail.com>
2025-01-18 11:50:22 +0100

    add option to choose model

--- src/dtu_mlops_project/frontend.py
+++ src/dtu_mlops_project/frontend.py
@@ -29,16 +29,18 @@ def about_model(backend):
     return None
 
 
-def classify_image(image, backend):
+def classify_image_model(image, backend, dummy_model):
     """Send the image to the backend for classification."""
-    predict_url = f"{backend}/api/predict/"
+    if dummy_model:
+        predict_url = f"{backend}/api/predict/dummy/"
+    else:
+        predict_url = f"{backend}/api/predict/"
     files = {"image_file": ("uploaded_image", image, image.type)}
     #files = {"image_file": open(image.name, "rb")}
     response = requests.post(predict_url, files=files, timeout=10)
     if response.status_code == 200:
         return response.json()
     return None
-
 
 def main() -> None:
     """Main function of the Streamlit frontend."""
@@ -56,7 +58,7 @@ def main() -> None:
     """)
 
     # add button to get information about the model
-    st.header("Model Used")
+    st.header("Trained Model Used")
     model_info = about_model(backend)
     if model_info is not None:
         st.write(model_info["model_name"])
@@ -66,44 +68,54 @@ def main() -> None:
     # Instructions
     st.header("Instructions")
     st.write("""
-        1. Click on the "Browse files" button to upload an image.
-        2. The image should be in JPEG, PNG, SVG, or SVG+XML format.
-        3. Once uploaded, the model will classify the image and display the results.
+        1. Choose the model to use (Trained Model or Dummy Model).
+        2. Upload an image of a vehicle (car, truck, etc.). Only JPEG, PNG, and SVG images are supported.
+        3. Click the 'Classify' button to get the prediction results.
     """)
 
-    # File Uploader
-    st.header("Upload an Image")
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpeg", "png", "svg", "svg+xml"])
+    # Choose dummy model or Trained model
+    st.header("Choose Model")
+    model_choice = st.selectbox("Select the model to use", ["Trained Model", "Dummy Model"], index=None, placeholder="Select model...")
+    if model_choice == "Dummy Model":
+        dummy_model = True
+    else:
+        dummy_model = False
 
-    if uploaded_file is not None:
-        # Get the MIME type of the uploaded file
-        mime_type, _ = mimetypes.guess_type(uploaded_file.name)
+    # Show the file uploader only if a model has been chosen
+    if model_choice:
+        # File Uploader
+        st.header("Upload an Image")
+        uploaded_file = st.file_uploader("Choose an image...", type=["jpeg", "png", "svg", "svg+xml"])
 
-        # Check if the MIME type is in the accepted types
-        if mime_type not in ["image/jpeg", "image/png", "image/svg+xml"]:
-            st.write("Invalid file type! Please upload an image of type JPEG, PNG, or SVG.")
+        if uploaded_file is not None:
+            # Get the MIME type of the uploaded file
+            mime_type, _ = mimetypes.guess_type(uploaded_file.name)
 
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_container_width=True)
-        st.write("Classifying...")
+            # Check if the MIME type is in the accepted types
+            if mime_type not in ["image/jpeg", "image/png", "image/svg+xml"]:
+                st.write("Invalid file type! Please upload an image of type JPEG, PNG, or SVG.")
 
-        result = classify_image(uploaded_file, backend=backend)
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Image", use_container_width=True)
+            st.write("Classifying...")
 
-        if result is not None:
-            probabilities = result["probabilities"]
+            result = classify_image_model(uploaded_file, backend=backend, dummy_model=dummy_model)
 
-            # Results
-            st.header("Results")
-            st.write("Prediction Probabilities:")
+            if result is not None:
+                probabilities = result["probabilities"]
 
-            # Bar Chart
-            data = {"Class": list(probabilities.keys()), "Probability": list(probabilities.values())}
-            df = pd.DataFrame(data)
-            df.set_index("Class", inplace=True)
-            st.bar_chart(df, y="Probability")
-        else:
-            st.write("Failed to classify the image")
+                # Results
+                st.header("Results")
+                st.write("Prediction Probabilities:")
 
+                # Bar Chart
+                data = {"Class": list(probabilities.keys()), "Probability": list(probabilities.values())}
+                df = pd.DataFrame(data)
+                df.set_index("Class", inplace=True)
+                st.bar_chart(df, y="Probability")
+            else:
+                st.write("Failed to classify the image")
+            
     # Resources
     st.header("Resources")
     st.write("""
