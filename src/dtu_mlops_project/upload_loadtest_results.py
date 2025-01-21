22dd2bfd0092670dc0d8118b336ff0ff2770715f
HackTheOxidation <tomas.hagenau@protonmail.ch>
2025-01-21 23:00:27 +0100

    Make a utility script for uploading locust load test results to W&B

---
+++ src/dtu_mlops_project/upload_loadtest_results.py
@@ -0,0 +1,74 @@
+"""
+This module is a utility script for uploading load testing results to W&B.
+"""
+
+import csv
+import sys
+from datetime import date
+from typing import Annotated
+
+import pandas
+from loguru import logger
+import typer
+import wandb
+import wandb_workspaces.reports.v2 as wr
+
+
+app = typer.Typer()
+run = wandb.init(project="dtu_mlops_project", job_type="add-dataset")
+today = date.today()
+
+
+@app.command()
+def upload_results(
+    result_stats: str = "./result_stats.csv",
+    result_failures: str = "./result_failures.csv",
+    result_exceptions: str = "./result_exceptions.csv",
+    result_stats_history: str = "./result_stats_history.csv",
+) -> None:
+    """
+    Uploads the 4 .csv files containing the data from the results of the
+    locust load tests. The files will be uploaded to W&B as a single
+    artifact under the name 'locust-loadtest-<date-of-today>'.
+
+    :param result_stats: Path to the 'result_stats.csv' file
+    :param result_failures: Path to the 'result_failures.csv' file
+    :param result_exceptions: Path to the 'result_exceptions.csv' file
+    :param result_stats_history: Path to the 'result_stats_history.csv' file
+    """
+    loadtest_result_artifact = wandb.Artifact(name=f"locust-loadtest-{today}", type="dataset")
+
+    loadtest_result_artifact.add_file(local_path=result_stats, name="result_stats")
+    loadtest_result_artifact.add_file(local_path=result_failures, name="result_failures")
+    loadtest_result_artifact.add_file(local_path=result_exceptions, name="result_exceptions")
+    loadtest_result_artifact.add_file(local_path=result_stats_history, name="result_stats_history")
+
+    loadtest_result_artifact.save()
+
+    logger.info(f"Successfully uploaded artifact: 'locust-loadtest-{today}' to W&B.")
+
+
+@app.command()
+def create_report(
+    result_stats: str = "./result_stats.csv",
+    result_failures: str = "./result_failures.csv",
+    result_exceptions: str = "./result_exceptions.csv",
+    result_stats_history: str = "./result_stats_history.csv",
+) -> None:
+    """
+    Creates a report in W&B covering the latest load test performed
+    using locust.
+
+    :param result_stats: Path to the 'result_stats.csv' file
+    :param result_failures: Path to the 'result_failures.csv' file
+    :param result_exceptions: Path to the 'result_exceptions.csv' file
+    :param result_stats_history: Path to the 'result_stats_history.csv' file
+    """
+    loadtest_report = wr.Report(
+        title=f"Locust load test results {today}",
+        description=f"Details about the locust load test result from {today}",
+    )
+
+
+if __name__ == "__main__":
+    app()
